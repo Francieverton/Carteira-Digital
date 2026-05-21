fe7256dfda55de6fd909cd661bedeae5e317f0fb
Francieverton <francieverton.so@gmail.com>
2026-05-21 09:58:00 -0300

    initial project

---
+++ src/main/java/com/francieverton/digital_wallet/WalletDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.francieverton.digital_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/francieverton/digital_wallet/WalletDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francieverton.digital_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
