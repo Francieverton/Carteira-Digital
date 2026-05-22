4ddf2e75fce2ea6b5037a19970da0892434c4763
Francieverton <francieverton.so@gmail.com>
2026-05-21 22:04:16 -0300

    feat: Adicionando CarteiraService com o método de sacar

---
+++ src/main/java/com/francieverton/digital_wallet/service/CarteiraService.java
@@ -0,0 +1,43 @@
+package com.francieverton.digital_wallet.service;
+
+import com.francieverton.digital_wallet.model.Carteira;
+import com.francieverton.digital_wallet.model.TipoTransacao;
+import com.francieverton.digital_wallet.model.Transacao;
+import com.francieverton.digital_wallet.repository.CarteiraRepository;
+import com.francieverton.digital_wallet.repository.TransacaoRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class CarteiraService {
+
+    private final CarteiraRepository carteiraRepository;
+    private final TransacaoRepository transacaoRepository;
+
+    @Transactional
+    public void sacar (Long carteiraId, BigDecimal valor) {
+
+        Carteira carteira = carteiraRepository.findById(carteiraId).orElseThrow(()
+                -> new RuntimeException("Carteira não encontrada"));
+
+        if (carteira.getSaldo().compareTo(valor) < 0){
+            throw new RuntimeException("Saldo insuficiente!");
+        }
+        else {
+            carteira.setSaldo(carteira.getSaldo().subtract(valor));
+        }
+
+        Transacao transacao = new Transacao();
+
+        carteiraRepository.save(carteira);
+
+        transacao.setTipo(TipoTransacao.SAQUE);
+        transacao.setValor(valor);
+        transacao.setCarteiraOrigem(carteira);
+        transacaoRepository.save(transacao);
+    }
+}
