a694daef2aeb5192054686f05425a3c25c8c48c1
Francieverton <francieverton.so@gmail.com>
2026-05-28 10:28:05 -0300

    feat: implementa regra de extrato paginado no repository e service

--- src/main/java/com/francieverton/digital_wallet/repository/TransacaoRepository.java
+++ src/main/java/com/francieverton/digital_wallet/repository/TransacaoRepository.java
@@ -1,9 +1,13 @@
 package com.francieverton.digital_wallet.repository;
 
 import com.francieverton.digital_wallet.model.Transacao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+
+    Page<Transacao> findByCarteiraOrigemIdOrCarteiraDestinoId(Long origemId, Long destinoId, Pageable pageable);
 }
